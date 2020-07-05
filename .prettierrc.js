3a2b408a883ba4499da315836d7a37fcc1250a4b
David Obodo <obododavid5@gmail.com>
2020-07-05 09:35:33 +0100

    feat: add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "tabWidth": 2,
+    "semi": true,
+    "singleQuote": false,
+    "bracketSpacing": false,
+    "jsxBracketSameLine": true,
+    "printWidth": 170,
+    "useTabs": true
+}
